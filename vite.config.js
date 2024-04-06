cfda53f67b5be005ec3b914a4bbecbc9246f3a1b
nhictu <nhib1910424@student.ctu.edu.vn>
2024-04-06 13:04:52 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      }
+    }
   },
   plugins: [
     vue(),
