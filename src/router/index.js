3811b8a029fcfa50501f09c8a6e82e2d8e7a82eb
nhictu <nhib1910424@student.ctu.edu.vn>
2024-04-07 10:48:56 +0700

    Cai dat trang loi 404

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import ContactBook from "@/views/ContactBook.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+    },
+];
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+export default router;
